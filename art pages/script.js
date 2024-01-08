e23c58ed95710d1206dd970625535d1628c1a312
sarashaikhh <155704588+sarashaikhdesigns@users.noreply.github.com>
2024-01-07 19:05:34 -0500

    slideshowww

--- art pages/script.js
+++ art pages/script.js
@@ -9,39 +9,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-// slideshow
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  const prevBtn = document.getElementById('prev');
-  const nextBtn = document.getElementById('next');
-  const slider = document.getElementById('image-slider');
-
-  let currentIndex = 0;
-
-  prevBtn.addEventListener('click', function () {
-    if (currentIndex > 0) {
-      currentIndex--;
-    } else {
-      currentIndex = slider.children.length - 1;
-    }
-    updateSlider();
-  });
-
-  nextBtn.addEventListener('click', function () {
-    if (currentIndex < slider.children.length - 1) {
-      currentIndex++;
-    } else {
-      currentIndex = 0;
-    }
-    updateSlider();
-  });
-
-  function updateSlider() {
-    const translateValue = -currentIndex * 100 + '%';
-    slider.style.transform = 'translateX(' + translateValue + ')';
-  }
+// slideshow//
+document.addEventListener("DOMContentLoaded", function () {
+  showSlides("slideshow-container");
+  showSlides("slideshow-container2");
 });
 
+function showSlides(containerId) {
+  let slideIndex = 0;
+  const slides = document.querySelector(`.${containerId}`).getElementsByClassName("fade");
 
+  function displaySlides() {
+      for (let i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
+
+      slideIndex++;
+
+      if (slideIndex > slides.length) {
+          slideIndex = 1;
+      }
+
+      slides[slideIndex - 1].style.display = "block";
+      setTimeout(displaySlides, 2000); // Change slide every 2 seconds
+  }
+
+  displaySlides();
+}
